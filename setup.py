ae5bf7bd65bd331a8f2e831079fe57b2273183ec
Alex Thompson <godfoder@acis.ufl.edu>
2015-03-05 16:52:53 -0500

    Fix MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+
+from setuptools import setup, find_packages
+
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
+
+setup(
+    name='idigbio',
+    version='0.1.0',
+    description='Python Client of iDigBio Search API',
+    long_description=(read('README.rst') + '\n\n' +
+                      read('HISTORY.rst') + '\n\n' +
+                      read('AUTHORS.rst')),
+    url='http://github.com/idigbio/idigbio-python-client/',
+    license='MIT',
+    author='Alex Thompson',
+    author_email='godfoder@acis.ufl.edu',
+    packages=find_packages(exclude=['tests*']),
+    install_requires=['pandas', 'requests'],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
